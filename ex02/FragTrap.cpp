8fc10de6a3670ac3ae1ff9773831e53ee8a7e80d
elsa2491 <elsa.touma@gmail.com>
2024-10-29 21:53:47 +0100

    ex02 done

--- ex02/FragTrap.cpp
+++ ex02/FragTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: eltouma <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 00:24:17 by eltouma           #+#    #+#             */
-/*   Updated: 2024/10/29 02:56:14 by eltouma          ###   ########.fr       */
+/*   Updated: 2024/10/29 21:15:02 by eltouma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,11 +50,15 @@ FragTrap& FragTrap::operator=(const FragTrap& rhs)
 
 void	FragTrap::highFivesGuys(void)
 {
-	if (!this->_energyPoints || !this->_hitPoints)
+	if (!this->_energyPoints)
 	{
-		std::cout << "❌ Sorry " << this->_name << " no high five for the weak!" << std::endl;
-		return ;
-//		throw std::invalid_argument("");
+		std::cout << "❌ Sorry " << this->_name << " no high five for the weaks!" << std::endl;
+		throw std::invalid_argument("");
+	}
+	if (!this->_hitPoints)
+	{
+		std::cout << "❌ Sorry " << this->_name << " no high five for dead people!" << std::endl;
+		throw std::invalid_argument("");
 	}
 	std::cout << std::endl << this->_name << " requests a high five!" << std::endl;
 }
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: eltouma <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 23:30:44 by eltouma           #+#    #+#             */
-/*   Updated: 2024/10/29 03:08:41 by eltouma          ###   ########.fr       */
+/*   Updated: 2024/10/29 21:42:02 by eltouma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,37 +14,47 @@
 #include "ScavTrap.hpp"
 #include "FragTrap.hpp"
 
+/*
+   If you want them to lose all their points, add this line before attack() call
+  	 for (int i = 0; i < 100; i++)
+
+
+   If you want to kill them, add this line before takeDamage() call
+  	 for (int i = 0; i < 5; i++)
+
+
+   If you want to create a FragTrap by copy, add this code outside the loop
+	   FragTrap	fragTrapCopy;
+	   fragTrapCopy = FragTrap("Seigneur Bohort");
+	   fragTrapCopy.ClapTrap::attack(name[0]);
+
+
+   If you want to create a ScavTrap copy, and attack it with FragTrap copy, add this code outside the loop
+	   FragTrap	fragTrapCopy;
+	   fragTrapCopy = FragTrap("Seigneur Bohort");
+	   fragTrapCopy.ClapTrap::attack(name[0]);
+	   ScavTrap	scavTrapCopy;
+	   scavTrapCopy = ScavTrap("Un lapin adulte");
+	   scavTrapCopy.attack(fragTrapCopy.getName());
+
+  	 // If you want to see Un lapin adulte as a guard keeper, add this line into the try()	
+	   scavTrapCopy.guardGate();
+ */
+
 int	main(void)
 {
-	const char *name[] = {"Jackson", "Jackson", "Jess", "Fabrice", "Elzu", "Mateo", "Mathieu", NULL};
+	const char *name[] = {"Jackson", "Jess", NULL}; 
 	int	length = tab_size(name);
 
 	FragTrap fragtrap[length];
-
-	FragTrap	fragTrapCopy;
-	fragTrapCopy = FragTrap(fragtrap[0]);
-	fragTrapCopy.ClapTrap::attack(fragtrap[1].getName());
-
-	ScavTrap	scavTrapCopy("ok");
-	scavTrapCopy = ScavTrap(name[0]);
 	for (int i = 0; name[i]; i++)
 		fragtrap[i] = FragTrap(name[i]);
 	for (int j = 0; name[j]; j++)
 	{
 		try {
-			scavTrapCopy.guardGate();
-			for (int i = 0; i < 12; i++)
-				fragtrap[j].beRepaired(12);
-			for (int i = 0; i < 22; i++)
-				fragtrap[j].attack(fragtrap[j + 1].getName());
-			for (int i = 0; i < 33; i++)
-				fragtrap[j].attack(fragtrap[j + 1].getName());
-			for (int i = 0; i < 3; i++)
-				fragtrap[j].takeDamage(12);
-			for (int i = 0; i < 3; i++)
-				fragtrap[j].ClapTrap::attack(fragtrap[j + 1].getName());
-			for (int i = 0; i < 32; i++)
-				fragtrap[j].beRepaired(12);
+			fragtrap[j].beRepaired(12);
+			fragtrap[j].attack(fragtrap[j + 1].getName());
+			fragtrap[j].takeDamage(50);
 		}
 		catch (std::invalid_argument& e)
 		{
@@ -52,99 +62,6 @@ int	main(void)
 		}
 		try {
 			fragtrap[j].highFivesGuys();
-		}
-		catch (std::invalid_argument& e)
-		{
-			std::cerr << e.what();
-		}
-	}
-	return (0);
-}
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: eltouma <marvin@42.fr>                     +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/28 23:30:44 by eltouma           #+#    #+#             */
-/*   Updated: 2024/10/29 14:53:24 by eltouma          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include "ClapTrap.hpp"
-#include "ScavTrap.hpp"
-
-int	main(void)
-{
-	const char *name[] = {"Jackson", "Jackson", "Jess", "Birdner", NULL};
-	int	length = tab_size(name);
-
-	ScavTrap scavtrap[length];
-
-	ScavTrap	copy;
-	copy = ScavTrap(scavtrap[0]);
-	copy.ClapTrap::attack(scavtrap[1].getName());
-	for (int i = 0; name[i]; i++)
-		scavtrap[i] = ScavTrap(name[i]);
-	for (int j = 0; name[j]; j++)
-	{
-		try {
-			scavtrap[j].guardGate();
-			for (int i = 0; i < 2; i++)
-				scavtrap[j].beRepaired(12);
-			for (int i = 0; i < 3; i++)
-				scavtrap[j].takeDamage(3);
-			for (int i = 0; i < 3; i++)
-				scavtrap[j].ClapTrap::attack(scavtrap[j + 1].getName());
-			for (int i = 0; i < 20; i++)
-				scavtrap[j].attack(scavtrap[j + 1].getName());
-			for (int i = 0; i < 25; i++)
-				scavtrap[j].beRepaired(2);
-		}
-		catch (std::invalid_argument& e)
-		{
-			std::cerr << e.what();
-		}
-	}
-	return (0);
-}
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: eltouma <marvin@42.fr>                     +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/28 23:30:44 by eltouma           #+#    #+#             */
-/*   Updated: 2024/10/29 18:02:27 by eltouma          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include "ClapTrap.hpp"
-
-/*
-If you want to kill them, add this ligne before takeDamage() call
-	for (int i = 0; i < 5; i++)
-
-If you want them to lose all their points, add thisline before attack() call
-	for (int i = 0; i < 15; i++)
-*/
-
-int	main(void)
-{
-	const char *name[] = {"Jackson", "Jess", NULL};
-	int	length = tab_size(name);
-
-	ClapTrap claptrap[length];
-	for (int i = 0; name[i]; i++)
-		claptrap[i] = ClapTrap(name[i]);
-	for (int j = 0; name[j]; j++)
-	{
-		try {
-			claptrap[j].beRepaired(2);
-			claptrap[j].attack(claptrap[j + 1].getName());
-			claptrap[j].takeDamage(15);
 		}
 		catch (std::invalid_argument& e)
 		{
