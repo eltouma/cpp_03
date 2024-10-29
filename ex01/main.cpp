fda006ab5465aa3d32f53081642dd5f5a0766045
elsa2491 <elsa.touma@gmail.com>
2024-10-29 21:59:18 +0100

    ex01 clear main()

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: eltouma <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 23:30:44 by eltouma           #+#    #+#             */
-/*   Updated: 2024/10/29 20:55:09 by eltouma          ###   ########.fr       */
+/*   Updated: 2024/10/29 21:58:23 by eltouma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,27 @@
 #include "ScavTrap.hpp"
 
 /*
-If you want to kill them, add this line before takeDamage() call
-	for (int i = 0; i < 5; i++)
+   If you want them to lose all their points, add this line before attack() call
+	   for (int i = 0; i < 50; i++)
 
-If you want them to lose all their points, add this line before attack() call
-	for (int i = 0; i < 50; i++)
+   If you want to kill them, add this line before takeDamage() call
+	   for (int i = 0; i < 5; i++)
 
-If you want to call attack() from ClapTrap, add this line into try()
-	scavtrap[j].ClapTrap::attack(scavtrap[j + 1].getName());
-*/
+   If you want to call attack() from ClapTrap, add this line into try()
+	   scavtrap[j].ClapTrap::attack(scavtrap[j + 1].getName());
+
+   If you want to create a ScavTrap copy, add this code outside loop
+	   ScavTrap	copy;
+	   copy = ScavTrap("Un lapin adulte");
+	   copy.ClapTrap::attack("Seigneur Bohort");
+ */
 
 int	main(void)
 {
-	const char *name[] = {"Jackson", "Jackson", "Jess", "Birdner", NULL};
+	const char *name[] = {"Jackson", "Jess", NULL};
 	int	length = tab_size(name);
 
 	ScavTrap scavtrap[length];
-
-	ScavTrap	copy;
-	copy = ScavTrap(scavtrap[0]);
-	copy.ClapTrap::attack(scavtrap[1].getName());
 	for (int i = 0; name[i]; i++)
 		scavtrap[i] = ScavTrap(name[i]);
 	for (int j = 0; name[j]; j++)
